b5e11b066e4c60ed6db3f1f5056e9c06323e7bed
jim3 <jimmy3.xyz@gmail.com>
2024-07-04 13:10:37 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,26 +1,33 @@
-# Uncomment this to pass the first stage
 import socket
-
 
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
+
     while True:
         try:
-            # Accept a connection
-            client_socket, client_address = server_socket.accept()
+            conn, client_address = server_socket.accept()
             print(f"Connection from {client_address} has been established.")
+            
+            res202 = b"HTTP/1.1 200 OK\r\n\r\n"
+            res404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
-            # Send HTTP response to the client
-            response = b"HTTP/1.1 200 OK\r\n\r\n"
-            client_socket.sendall(response)
+            recv_data = conn.recv(1024)
+            http_request = recv_data.decode('utf-8').splitlines() # -> list[str]
+            request_line = http_request[0]
+            
+            for v in request_line:
+                split_str = request_line.split(' ') # -> list[str]
+                if(split_str[1] != '/'):
+                    conn.sendall(res404)
+                elif(split_str[1]=='/'):
+                    conn.sendall(res202)
+                else:
+                    None
+
         finally:
-            # Close the client connection regardless of exceptions
-            client_socket.close()
+            conn.close()
             print(f"Connection from {client_address} has been closed.")
-
-# --------------------------------------------- #
 
 if __name__ == "__main__":
     main()
