0250df23d4a63572970428d6681c69f1d766dbd9
jim3 <jimmy3.xyz@gmail.com>
2024-07-05 18:18:07 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,44 +1,63 @@
 import socket
 import re
 
+
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
     while True:
         try:
-            conn, client_address = server_socket.accept()
+            conn, client_address = (
+                server_socket.accept()
+            )  # Wait for an incoming connection
             print(f"Connection from {client_address} has been established...")
-            # receive data from the socket. return value is a bytes object
-            recv_data = conn.recv(1024)
-            http_request = recv_data.decode('utf-8').splitlines() # -> list[str]
+
+            try:
+                recv_data = conn.recv(
+                    1024
+                )  # receive data from the socket -> bytes object
+                http_request = recv_data.decode(
+                    "utf-8"
+                ).splitlines()  # decode bytes into a string -> list[str]
+            except UnicodeDecodeError:
+                print(f"Error decoding data from client {client_address}")
 
             # Extract the request path
             request_line = http_request[0]
-            split_str = request_line.split(' ')
+            split_str = request_line.split(" ")
 
             if len(split_str) < 2:
                 continue
 
-            path= split_str[1]
+            path = split_str[1]
 
             res200 = b"HTTP/1.1 200 OK\r\n\r\n"
             res404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
-            if path == '/':
+            if path == "/":
                 conn.sendall(res200)
-            elif path.startswith('/echo/'):
+            elif path.startswith("/echo/"):
                 pattern = r"/echo/(\S+)"
                 match = re.search(pattern, request_line)
 
                 if match:
-                    str_result = match.group(1) # print(f"Match found: {str_result}")
-                    
+                    str_result = match.group(1)  # print(f"Match found: {str_result}")
+
                     # Gather all of our responses
-                    response_body = f"{str_result}".encode('utf-8') # Encode response_body to utf-8 bytes
+                    response_body = f"{str_result}".encode(
+                        "utf-8"
+                    )  # Encode response_body to utf-8 bytes
                     status_line = b"HTTP/1.1 200 OK\r\n"
                     content_type = b"Content-Type: text/plain\r\n"
-                    content_length = f"Content-Length: {len(response_body)}\r\n".encode('utf-8')
-                    response_headers = (content_type + content_length) # Create the header response
-                    response = status_line + response_headers + b"\r\n" + response_body # Create the response
+                    content_length = f"Content-Length: {len(response_body)}\r\n".encode(
+                        "utf-8"
+                    )
+                    response_headers = (
+                        content_type + content_length
+                    )  # Create the header response
+                    response = (
+                        status_line + response_headers + b"\r\n" + response_body
+                    )  # Create the response
 
                     # send the response back to the client
                     conn.sendall(response)
@@ -51,6 +70,7 @@ def main():
         finally:
             conn.close()
             print(f"Connection from {client_address} has been closed.")
+
 
 if __name__ == "__main__":
     main()
