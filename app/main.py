78417d015a3da4873c35e24df0bacae009dca452
jim3 <jimmy3.xyz@gmail.com>
2024-07-05 18:19:03 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import socket
 import re
 
+
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
@@ -10,35 +11,39 @@ def main():
             conn, client_address = server_socket.accept()
             print(f"Connection from {client_address} has been established.")
             recv_data = conn.recv(1024)
-            http_request = recv_data.decode('utf-8').splitlines() # -> list[str]
+            http_request = recv_data.decode("utf-8").splitlines()  # -> list[str]
             # Extract the request path from the request line
             request_line = http_request[0]
-            split_str = request_line.split(' ')
+            split_str = request_line.split(" ")
 
             if len(split_str) < 2:
                 continue
 
-            path= split_str[1]
+            path = split_str[1]
             res200 = b"HTTP/1.1 200 OK\r\n\r\n"
             res404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
-            if path == '/':
+            if path == "/":
                 conn.sendall(res200)
-            elif path.startswith('/echo/'):    
+            elif path.startswith("/echo/"):
                 pattern = r"/echo/(\S+)"
                 match = re.search(pattern, request_line)
                 if match:
                     str_result = match.group(1)
                     print(f"Match found: {str_result}")
                     # Gather all of our responses
-                    response_body = f"{str_result}".encode('utf-8') # Encode response_body to utf-8 bytes
+                    response_body = f"{str_result}".encode(
+                        "utf-8"
+                    )  # Encode response_body to utf-8 bytes
                     status_line = b"HTTP/1.1 200 OK\r\n"
                     content_type = b"Content-Type: text/plain\r\n"
-                    content_length = f"Content-Length: {len(response_body)}\r\n".encode('utf-8')
+                    content_length = f"Content-Length: {len(response_body)}\r\n".encode(
+                        "utf-8"
+                    )
                     # Create the header response
-                    response_headers = (content_type + content_length)                   
+                    response_headers = content_type + content_length
                     # Create the response
-                    response = status_line + response_headers + b"\r\n" + response_body                   
+                    response = status_line + response_headers + b"\r\n" + response_body
                     # send the response back to the client
                     conn.sendall(response)
                 else:
@@ -46,10 +51,11 @@ def main():
             else:
                 conn.sendall(res404)
         except Exception as e:
-            print(f"An error occurred: {e}")            
+            print(f"An error occurred: {e}")
         finally:
             conn.close()
             print(f"Connection from {client_address} has been closed.")
+
 
 if __name__ == "__main__":
     main()
