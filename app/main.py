d0ff278705017f7b13d2741ad4125b13d57cf1e3
jim3 <jimmy3.xyz@gmail.com>
2024-07-05 11:49:00 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,34 +2,28 @@ import socket
 import re
 
 def main():
-    print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
         try:
             conn, client_address = server_socket.accept()
-            print(f"Connection from {client_address} has been established.")
+            print(f"Connection from {client_address} has been established...")
+            # receive data from the socket. return value is a bytes object
             recv_data = conn.recv(1024)
             http_request = recv_data.decode('utf-8').splitlines() # -> list[str]
-            print(f"HTTP Request: {http_request}")
-
-            # Extract the request path from the request line
+            # Extract the request path
             request_line = http_request[0]
-            print(f"Request Line: {request_line}")
             split_str = request_line.split(' ')
-
             if len(split_str) < 2:
                 continue
             path= split_str[1]
 
-            # responses 200/404
             res200 = b"HTTP/1.1 200 OK\r\n\r\n"
             res404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
-
-
+            
             if path == '/':
                 conn.sendall(res200)
-            elif path.startswith('/echo/'):    
+            elif path.startswith('/echo/'):
                 pattern = r"/echo/(\S+)"
                 match = re.search(pattern, request_line)
 
@@ -42,13 +36,10 @@ def main():
                     status_line = b"HTTP/1.1 200 OK\r\n"
                     content_type = b"Content-Type: text/plain\r\n"
                     content_length = f"Content-Length: {len(response_body)}\r\n".encode('utf-8')
-
                     # Create the header response
                     response_headers = (content_type + content_length)
-                    
                     # Create the response
                     response = status_line + response_headers + b"\r\n" + response_body
-                    
                     # send the response back to the client
                     conn.sendall(response)
                 else:
@@ -56,7 +47,7 @@ def main():
             else:
                 conn.sendall(res404)
         except Exception as e:
-            print(f"An error occurred: {e}")            
+            print(f"An error occurred: {e}")
         finally:
             conn.close()
             print(f"Connection from {client_address} has been closed.")
