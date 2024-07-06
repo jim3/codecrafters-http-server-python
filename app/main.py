1545c994f29aaea5e014fe89d0dca7ba28495c58
jim3 <jimmy3.xyz@gmail.com>
2024-07-06 11:34:42 -0400

    update main.py

--- app/main.py
+++ app/main.py
@@ -2,63 +2,66 @@
 import socket
 import re
 
+BUFF_SZ = 1024
+ENC = "utf-8"
+
 
 def handle_connection(conn, client_address):
     print(f"Connection from {client_address} has been established...")
-    try:
-        recv_data = conn.recv(1024)
-        http_request = recv_data.decode("utf-8").splitlines()
-        print(f"Request from {client_address}:\n{http_request}")
+    data = conn.recv(BUFF_SZ)
+    request = data.decode(ENC)
+    print(f"Request from {client_address}:\n{request}")
+    # http_method, path, user_agent, body = parse_request(request)
 
-        # PARSE THE REQUEST
-        response = parse_request(conn, http_request)
+    # call parse_request
+    response = parse_request(request)
 
-        # SEND RESPONSE TO main FUNCTION
-        # ...
+    conn.sendall(response().encode(ENC))
+    conn.close()
+    # PARSE THE REQUEST
+    # response = parse_request(conn, http_request)
 
-    except UnicodeDecodeError:
-        print(f"Error decoding data from client {client_address}")
-    finally:
-        conn.close()
-        print(f"Connection from {client_address} has been closed.")
+    # SEND RESPONSE TO main FUNCTION
+    # send_response_back(responose)
 
 
 def parse_request(conn, http_request):
     res200 = b"HTTP/1.1 200 OK\r\n\r\n"
     res404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
-    # Extract path from http_request[] ['GET / HTTP/1.1', [1], [2]]
-    request_line = http_request[0]  # 'GET / HTTP/1.1'
-    split_str = request_line.split(" ")
-
-    # Extract the path
-    path = split_str[1]  # `/` [`/echo/{str}/` | `/user-agent/`]
+    # request_line = http_request[0]  # get 1st element
+    # split_str = request_line.split(" ")
+    # path = split_str[1]  # use split to get `/` root path
 
     if path == "/":
+        request_line = http_request[0]  # get 1st element
+        split_str = request_line.split(" ")
+        path = split_str[1]  # use split to get `/` root path
         conn.sendall(res200)
     elif path.startswith("/echo/"):
         pattern = r"/echo/(\S+)"
         match = re.search(pattern, path)
-
         if match:
             str_result = match.group(1)  # print(f"Match found: {str_result}")
-
-            # Gather all of the responses
-            response_body = f"{str_result}".encode("utf-8")
-            status_line = b"HTTP/1.1 200 OK\r\n"
-            content_type = b"Content-Type: text/plain\r\n"
-            content_length = f"Content-Length: {len(response_body)}\r\n".encode("utf-8")
-
-            # Create the header response
-            response_headers = content_type + content_length
-            response = status_line + response_headers + b"\r\n" + response_body
-            # return f"HTTP/1.1 {code} OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(content)}\r\n\r\n{content}"
-
-            # return response
-            return response
+            return str_result
         else:
             conn.sendall(res404)
         conn.close()
+
+
+def response(str_result):
+    # Gather all of the responses
+    response_body = f"{str_result}".encode("utf-8")
+    status_line = b"HTTP/1.1 200 OK\r\n"
+    content_type = b"Content-Type: text/plain\r\n"
+    content_length = f"Content-Length: {len(response_body)}\r\n".encode("utf-8")
+
+    # Create the header response
+    response_headers = content_type + content_length
+    response = status_line + response_headers + b"\r\n" + response_body
+
+    # return response
+    return response
 
 
 def main():
