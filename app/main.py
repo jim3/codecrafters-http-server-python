b46a9d531168770ca29e43e43cf031572092e7b3
jim3 <jimmy3.xyz@gmail.com>
2024-07-08 21:12:05 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,9 @@
 import socket
-import re
 
 BUFF_SZ = 1024
 ENC = "utf-8"
 RES200 = b"HTTP/1.1 200 OK\r\n\r\n"
 RES404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
-
-
-# f"HTTP/1.1 {status_code} OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}"
-def response(str_result):
-    response_body = f"{str_result}".encode("utf-8")
-    status_line = b"HTTP/1.1 200 OK\r\n"
-    content_type = b"Content-Type: text/plain\r\n"
-    content_length = f"Content-Length: {len(response_body)}\r\n".encode("utf-8")
-    response_headers = content_type + content_length
-    response = status_line + response_headers + b"\r\n" + response_body
-    return response
 
 
 def echo_string(request_line):
@@ -32,9 +20,18 @@ def get_user_agent(headers):
     return ""
 
 
-def parse_request(conn, http_request):
+def get_file_name(request_line):
+    file = request_line.split()
+    if len(file) >= 2 and file[0] == "GET" and file[1].startswith("/files/"):
+        return file[1][len("/files/") :]
+    return None
+
+
+def parse_request(http_request):
     request_line = http_request[0]
     headers = http_request[1:-2]
+    # request_lines = request_line.split("\r\n")
+    # http_method, path, _ = request_lines[0].split(" ", 2)
 
     if request_line == "GET / HTTP/1.1":
         return RES200
@@ -45,8 +42,24 @@ def parse_request(conn, http_request):
     elif request_line.startswith("GET /user-agent"):
         user_agent = get_user_agent(headers)
         return response(user_agent)
+    elif request_line.startswith("GET /files/"):
+        file_name = get_file_name(request_line)
+        if file_name:
+            return response(file_name)
     else:
         return RES404
+
+
+def response(str_result):
+    response_body = f"{str_result}".encode("utf-8")
+    res_body_length = len(response_body)  # Integer representing byte length
+    status_line = b"HTTP/1.1 200 OK\r\n"
+    content_type = b"Content-Type: application/octet-stream\r\n"
+    content_length = str(res_body_length).encode("utf-8")  # -> bytes
+    response_headers = content_type + content_length
+    response = status_line + response_headers + b"\r\n" + response_body
+    print("Printing entire response: ", response)
+    return response  # -> bytes
 
 
 def handle_connection(conn, client_address):
